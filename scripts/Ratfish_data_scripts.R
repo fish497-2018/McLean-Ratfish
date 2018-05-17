4fd78280ce9cf9ff88a4bab7346bf9b3202fdbf6
Jon McLean <Jon McLean>
2018-05-17 10:17:27 -0700

    added code for histogram

--- scripts/Ratfish_data_scripts.R
+++ scripts/Ratfish_data_scripts.R
@@ -2,5 +2,8 @@ ratfish_data <- read.csv("data/Copy of JonMcLean_Ratfish_Tow_Location_Densities(
 
 library(tidyverse)
 
-ratfish_data_rounded <- mutate(ratfish_data, round(StartLatitude_DD, digits = 2), round(StartLongitude_DD, digits = 2))
+ratfish_data_rounded <- mutate(ratfish_data, rounded_latitude = round(StartLatitude_DD, digits = 2), rounded_longitude = round(StartLongitude_DD, digits = 2)) 
 
+
+ggplot(data = ratfish_data_rounded) +
+  geom_histogram(aes(x = rounded_latitude, y = Ratfish_BiomassDensity.kg.ha.))
