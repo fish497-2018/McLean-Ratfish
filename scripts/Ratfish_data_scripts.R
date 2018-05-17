18fae73a1948532471e04eb96234537f2fa49167
Jon McLean <Jon McLean>
2018-05-17 09:45:52 -0700

    Added longitude to code

--- scripts/Ratfish_data_scripts.R
+++ scripts/Ratfish_data_scripts.R
@@ -2,5 +2,5 @@ ratfish_data <- read.csv("data/Copy of JonMcLean_Ratfish_Tow_Location_Densities(
 
 library(tidyverse)
 
-ratfish_data_rounded <- mutate(ratfish_data, round(StartLatitude_DD, digits = 2))
+ratfish_data_rounded <- mutate(ratfish_data, round(StartLatitude_DD, digits = 2), round(StartLongitude_DD, digits = 2))
 
