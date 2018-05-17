d51a5b643c75adae3b58dfe290f90ea8c25400ba
Jon McLean <Jon McLean>
2018-05-17 09:44:38 -0700

    Added column of rounded latitudes

--- scripts/Ratfish_data_scripts.R
+++ scripts/Ratfish_data_scripts.R
@@ -1,3 +1,6 @@
 ratfish_data <- read.csv("data/Copy of JonMcLean_Ratfish_Tow_Location_Densities(4824).csv")
 
 library(tidyverse)
+
+ratfish_data_rounded <- mutate(ratfish_data, round(StartLatitude_DD, digits = 2))
+
